1c4f6aa476908f911d08b79505ec3eeaf318e1c7
sen <sen@localhost.localdomain>
2018-02-26 10:30:24 -0500

    testing s/c install scripts

---
+++ geniInstalls/installC.sh
@@ -0,0 +1,29 @@
+﻿#!/bin/sh
+
+# Usual directory for downloading software in ProtoGENI hosts is `/local`
+cd /local
+
+##### Check if file is there #####
+if [ ! -f "./installed.txt" ]
+then
+       #### Create the file ####
+        sudo touch "./installed.txt"
+
+       #### Run  one-time commands ####
+       
+       #Install necessary packages
+        sudo apt update & EPID=$!
+        wait $EPID
+        sudo apt -y install git-core unzip cmake libpcap-dev libxerces-c2-dev libpcre3-dev flex bison g++ autoconf libtool pkg-config libboost-dev libboost1.40-all-dev gawk & EPID=$!
+        wait $EPID
+
+       # Install custom software
+       	sudo mkdir chatServer & EPID=$!
+	wait $EPID
+       	sudo wget http://mountrouidoux.people.cofc.edu/CyberPaths/chat/chatc2.py chatServer/ & EPID=$!
+       	wait $EPID
+       	
+       
+       ## Reboot the OS to run a latest kernel without IPv6.
+       sudo reboot
+fi
